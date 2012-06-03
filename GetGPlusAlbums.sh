42b8b034394b738fc9833a11cc87dfe4acbc2f4e
Jeroen <vanantwerpenjeroen@gmail.com>
2012-06-03 16:38:11 +0200

    Furder code clean-up, other way to detect name, preps multiple download

--- GetGPlusAlbums.sh
+++ GetGPlusAlbums.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -7,6 +7,7 @@
 # Copyright (C) 2012 by Mitesh Shah (Mr.Miteshah@gmail.com)
 # Special thanks to Daniel Sandman (revoltism@gmail.com)
 
+allowedExtensions="jpg png gif jpeg"
 #       Name of the script, a bit better then $0, since you don't always know
 #       how the script is being executed (absolute, relative, from /bin, ...)
 #       Doesn't really work well with linked names, but you get the point
@@ -27,11 +28,18 @@ tmpDir="/tmp/$program.d.$$"
 mkdir "$tmpDir"
 _clean_program()
 {
-	ls "$tmpDir"
+#	ls "$tmpDir"
         rm -r "$tmpDir"
+        trap - EXIT INT TERM QUIT ABRT KILL
         exit 0
 }
 trap "_clean_program" EXIT INT TERM QUIT ABRT KILL
+
+# A very simple error-method
+ownError(){
+	echo $@ >&2
+	exit 1
+}
 
 #Create A  FLNAME Function
 FLNAMES(){
@@ -43,103 +51,76 @@ FLNAMES(){
 	done
 
 	# Searching On Google For The Specified Names
-	GID="`curl -A 'Mozilla/4.0' --silent "https://www.google.com/search?q=site%3Aplus.google.com${append}" | grep -P -o '(?<=plus.google.com/)[^/u ]+(?=/)' | sed -n 1p`" || ownError "Get GID Failed"
-
+	GID="$(	\
+		curl -A 'Mozilla/4.0' --silent "https://www.google.com/search?q=site%3Aplus.google.com${append}" | \
+		grep -P -o '(?<=plus.google.com/)[^/u ]+(?=/)' | \
+		sed -n 1p \
+	)" || ownError "Get GID Failed"
 }
-
-
 #Create A GPlusID Function
 GPLUSID(){
 	GID="$1"
-	#Enter Google Plus ID
-	#read -p "Enter 21 Digit Google Plus Profile ID: " GID
-	#echo $GID
-}
-
-# A very simple error-method
-ownError(){
-	echo $@ >&2
-	exit 1
 }
 
 showHelp(){
-	#TODO: Fill in
-	cat <<-EOF_HELP
-	Please give some arguments
-	EOF_HELP
+cat <<EOF_HELP
+$program:	Download a photo-album from Google+
+	1st argument:
+		name:	Search by name
+		id:	Search by Google+-ID
+	2nd-... argument:
+		id or name of the person
+		(no " or \\ needed for spaces in names)
+EOF_HELP
 	exit 0
 }
 
-main(){
-if [ -z $2 ]
-then
-        clear
-        showHelp
-else
-	choise=$1
-	shift
-fi
-case $choise in
-        name) FLNAMES $@;;
-        id) GPLUSID $@;;
-        *) showHelp;;
-esac
+downloadAlbum(){
+	albumNr=$1
+	#Get The URL Of Selected Album
+	GPlusAlbum=$(sed -n "/$albumNr/p" $tmpDir/albums.html | awk -F ',' '{print $8}' | sed 's/"//g' | sed '/^$/d')
+	echo $GPlusAlbum
 
-# Download The Album Page Of $FNAME $LNAME Person
-wget -qc https://plus.google.com/photos/$GID/albums -O $tmpDir/albums.html   ||  ownError "Album not found"
-
-#Sorting Album Page So We Only Get Album Names
-OPTIONS=$(sed -n '/\[1\,\[\,1\,\[\"https/p' $tmpDir/albums.html | grep -P -o '(?<=/)[^/]+(?=\")' | sed 1d)
-
-#Making Album Menu
-select OPT in $OPTIONS;
-do
-	case $OPT in
-	*)
-		echo "You Selected $OPT ($REPLY)"
-		break
-		;;
-	esac
-done
-
-#Get The URL Of Selected Album
-GPlusAlbum=$(sed -n "/$OPT/p" $tmpDir/albums.html | awk -F ',' '{print $8}' | sed 's/"//g' | sed '/^$/d')
-echo $GPlusAlbum
-
-#Download Selected Album Contents
-wget -qc $GPlusAlbum -O $tmpDir/photos.html
+	#Download Selected Album Contents
+	wget -qc $GPlusAlbum -O $tmpDir/photos$albumNr.html
 
 #Get the Album-name
-GPlusAlbumName=$(sed '/data:/p' $tmpDir/photos.html | grep -P -o "(?<=/)[^/]+(?=#)" | sort -u | grep -v "<" | grep -v "?")
+GPlusAlbumName=$(sed '/data:/p' $tmpDir/photos$albumNr.html | grep -P -o "(?<=/)[^/]+(?=#)" | sort -u | grep -v "<" | grep -v "?")
 echo GPlus Album Name = $GPlusAlbumName
 
 #Generate FNAME & LNAME
 #TODO: Detect fault in this part
-NAME="$(sed '/data:/p' $tmpDir/photos.html | grep -P -o "(?<=,,)[^/150,]+(?=,)" | grep -i [a-z] | sort -u | head -n1 | cut -d"\"" -f2)"
-FNAME=$(echo "$NAME" | cut -d" " -f1)
-LNAME=$(echo "$NAME" | cut -d" " -f2)
+NAME="$(							\
+	grep "https://plus.google.com" $tmpDir/photos$albumNr.html |	\
+	grep "googleusercontent" |				\
+	cut -d'"' -f6 | grep -i [a-z] |				\
+	sort -u | head -n1 |					\
+	sed 's/ /_/g' 						\
+	)" #"' # Get colour-recognistion all right
 
 #Make directory using First and Last Name
-TargetDir="$FNAME$LNAME/$GPlusAlbumName"
+TargetDir="$NAME/$GPlusAlbumName"
 echo Target Directory = $TargetDir
 mkdir -p "$TargetDir"
 
-
 #Make an empthy file
 echo -n "" > $tmpDir/MiteshShah.txt
-
-for extension in jpg png gif jpeg
+for extension in $allowedExtensions
 do
-# Workflow: 
-# - cat file
-# - get lines containing EXTENSION
-# - cut-out right section
-# - check if EXTENSION is still in that part
-	cat $tmpDir/photos.html | grep -i $extension | cut -d'"' -f4 | grep -i $extension >> $tmpDir/MiteshShah.txt
+	# Workflow: 
+	# - cat file
+	# - get lines containing EXTENSION
+	# - cut-out right section
+	# - check if EXTENSION is still in that part
+	# - check if it starts with https, not //ssl
+	cat $tmpDir/photos$albumNr.html | grep -i $extension |	\
+		cut -d'"' -f4 | grep -i $extension |	\
+		grep -i ^https				\
+		>> $tmpDir/MiteshShah.txt
 done
 
 #Download the URL's
-if [ $(cat $tmpDir/MiteshShah.txt | wc) != 0 ]
+if [ "$(cat $tmpDir/MiteshShah.txt | wc -l)" != "0" ]
 then
 	#Download Starts
 	cd $TargetDir
@@ -147,7 +128,55 @@ then
 
 	#Remove Extra Unwanted Stuff
 	cd - &> /dev/null
+else
+	echo "No photos where found :("
 fi
 }
 
+main(){
+	if [ -z $2 ]
+	then
+		# Check if arguments are given
+		clear
+		showHelp
+	else
+		# Remember the choise, and remove is from the arguments
+		# makes use of $@ easier
+		choise=$1
+		shift
+	fi
+	case $choise in
+		name) FLNAMES $@;;
+		id) GPLUSID $@;;
+		*) showHelp;;
+	esac
+
+	# Download The Album Page Of $FNAME $LNAME Person
+	wget -qc https://plus.google.com/photos/$GID/albums -O $tmpDir/albums.html   ||  ownError "Album not found"
+
+	#Sorting Album Page So We Only Get Album Names
+	OPTIONS=$(sed -n '/\[1\,\[\,1\,\[\"https/p' $tmpDir/albums.html | grep -P -o '(?<=/)[^/]+(?=\")' | sed 1d)
+	echo $OPTIONS
+	sleep 1000
+	#Making Album Menu
+	select OPT in $OPTIONS;
+	do
+		case $OPT in
+		0)
+			echo "You selected every album"
+			for i in $(seq )
+			do
+				downloadAlbum $i &
+			done
+			wait
+			break
+			;;
+		*)
+			echo "You Selected $OPT ($REPLY)"
+			downloadAlbum $OPT
+			break
+			;;
+		esac
+	done
+}
 main $@
